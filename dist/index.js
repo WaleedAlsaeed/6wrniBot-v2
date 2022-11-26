0bfffa820ce73c1edc4f1f034d6e3c46e2a13eab
WaleedAlsaeed <waleed.gdev+gbh@gmail.com>
2022-11-26 15:22:34 +0300

    WWWW

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,7 +8,7 @@ require('dotenv').config();
 const Client_1 = require("./structures/Client");
 const consts_1 = require("./config/consts");
 const LevelSystem_1 = require("./config/LevelSystem");
-const axios_1 = __importDefault(require("axios"));
+var cron = require('node-cron');
 exports.client = new Client_1.ExtendedClient();
 exports.config = new consts_1.Config();
 exports.lvlsys = new LevelSystem_1.LevelSystem();
@@ -28,21 +19,9 @@ app.get("/", (req, res) => {
     res.status(200).send('OK');
 });
 app.listen(port, () => exports.client.start());
-function checkUpdates() {
-    setTimeout(() => __awaiter(this, void 0, void 0, function* () {
-        for (let i = 0; i < 5; i++) {
-            try {
-                const { data, status } = yield axios_1.default.get(process.env.UPDATE || "");
-                console.log(data);
-                console.log('response status is: ', status);
-                setTimeout(() => exports.client.destroy(), 150000);
-                return;
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-        exports.config.LogChannel("Unable to check updates");
-    }), 1200000);
-}
-checkUpdates();
+cron.schedule('19 * * * *', () => {
+    console.log('==> [Status]: Restart BOT...');
+    exports.client.destroy();
+    exports.client.start();
+    console.log('==> [Status]: Done.');
+});
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import { ExtendedClient } from './structures/Client';
 import { Config } from './config/consts';
 import { LevelSystem } from './config/LevelSystem';
-import axios from 'axios';
+var cron = require('node-cron');
 
 export const client = new ExtendedClient();
 export const config = new Config();
@@ -20,24 +20,9 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => client.start());
 
-
-function checkUpdates() {
-    setTimeout(async () => {
-        for (let i = 0; i < 5; i++) {
-            try {
-                const { data, status } = await axios.get(
-                    process.env.UPDATE || ""
-                );
-                console.log(data);
-                console.log('response status is: ', status);
-                setTimeout(() => client.destroy(), 150000)
-                return;
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        config.LogChannel("Unable to check updates");
-    }, 1200000);
-}
-
-checkUpdates();
+cron.schedule('19 * * * *', () => {
+  console.log('==> [Status]: Restart BOT...');
+  client.destroy();
+  client.start();
+  console.log('==> [Status]: Done.');
+});
